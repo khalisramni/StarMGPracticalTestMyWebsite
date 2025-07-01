dcb86c7127fc286a43e2347033e99f0cd3562c40
khalisramni <khalis6kb@yahoo.com>
2025-07-01 19:12:14 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db_host = 'localhost';
-$db_name = 'my_website';
-$db_user = 'website_user';
-$db_pass = 'new_password';
+$db_host = 'yourhost';
+$db_name = 'your_dbname';
+$db_user = 'your_username';
+$db_pass = 'your_password';
